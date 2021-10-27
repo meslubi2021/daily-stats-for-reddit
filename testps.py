eca5d19913cfc71fb196ad460ce17a61c30b1985
samu91gn <samuele8@gmail.com>
2021-10-27 22:49:01 +0200

    testingps

--- testps.py
+++ testps.py
@@ -1,10 +1,30 @@
 print("importo la merda")
 from psaw import PushshiftAPI
-        
+import praw
+import utils 
+import config_reader as config 
+
+print("fetching subs")
+CLIENT_ID = utils.get_env('CLIENT_ID') or config.get('PRAW', 'CLIENT_ID')
+CLIENT_SECRET = utils.get_env("CLIENT_SECRET") or config.get('PRAW', 'CLIENT_SECRET')
+USER_AGENT = config.get('GENERAL', 'USER_AGENT')
+
+reddit = praw.Reddit(
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            user_agent=USER_AGENT
+        )
+
+print("creato reddit")
+print("client id "+ CLIENT_ID)
+print("client sec "+ CLIENT_SECRET)
+print("user agent "+ USER_AGENT)
+
 
 print("creating api")
-psAPI = PushshiftAPI()
-print("fetching subs")
+psAPI = PushshiftAPI(reddit)
+
+
 subs = psAPI.search_submissions(after=1633821330,
                             before=1633824930,
                             subreddit="CryptoCurrency",
