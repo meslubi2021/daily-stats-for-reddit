da0a7fdf58e549ec5b2cc633ad7a82e8971b8d9e
samu91gn <samuele8@gmail.com>
2021-10-27 22:45:02 +0200

    testingps

--- testps.py
+++ testps.py
@@ -1,10 +1,16 @@
+print("importo la merda")
 from psaw import PushshiftAPI
         
-psAPI = PushshiftAPI()
 
-psAPI.search_submissions(after=1633821330,
+print("creating api")
+psAPI = PushshiftAPI()
+print("fetching subs")
+subs = psAPI.search_submissions(after=1633821330,
                             before=1633824930,
                             subreddit="CryptoCurrency",
                             filter=['url', 'id'])
 
-
+subs = list(subs)
+print("fetched "+str(len(subs))+" submissions")
+for sub in subs:
+    print("sub: "+sub.id)
